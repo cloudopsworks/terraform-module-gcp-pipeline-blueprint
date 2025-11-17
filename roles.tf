af85267faafc40f8077acc415f95b86f66a3c88a
Cristian Beraha <cristian@cloudops.works>
2025-11-17 16:19:08 -0300

    PIP-8 Add support for Google Cloud Run and GKE configurations, roles, and service accounts.

--- roles.tf
+++ roles.tf
@@ -25,5 +25,38 @@ locals {
       ]
     }
   ] : []
-  roles = concat(local.role_appengine)
+  role_cloudrun = var.cloudrun.enabled ? [
+    {
+      name_prefix = "cloudrun-deployer"
+      title       = "Cloud Run Deployer"
+      description = "Role to deploy Cloud Run services"
+      permissions = [
+        "run.services.create",
+        "run.services.update",
+        "run.services.delete",
+        "run.routes.get",
+        "run.routes.update",
+        "run.revisions.list",
+        "run.revisions.delete",
+      ]
+    }
+  ] : []
+  role_gke = var.gke.enabled ? [
+    {
+      name_prefix = "gke-deployer"
+      title       = "GKE Deployer"
+      description = "Role to deploy GKE Workloads"
+      permissions = [
+        "container.clusters.get",
+        "container.clusters.list",
+        "container.workloads.create",
+        "container.workloads.update",
+        "container.workloads.delete",
+        "container.pods.get",
+        "container.pods.list",
+      ]
+    }
+  ] : []
+  role_preview = []
+  roles        = concat(local.role_appengine, local.role_cloudrun, local.role_gke, local.role_preview)
 }
